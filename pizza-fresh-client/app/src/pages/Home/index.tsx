6487fa831bdad1ce741153d74e68cc038190f7a8
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-09-18 14:58:04 -0300

    feat: add lib Luxor and add DateTime

--- pizza-fresh-client/app/src/pages/Home/index.tsx
+++ pizza-fresh-client/app/src/pages/Home/index.tsx
@@ -1,10 +1,13 @@
 import Menu from "components/Menu";
+import  {DateTime} from "luxon"
 import { ReactComponent as Search} from "assets/icons/search.svg"
 import * as S from "./style";
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
 
+
 const Home = () => {
+  const  dateDescription = DateTime.now().toLocaleString({...DateTime.DATE_SHORT, weekday: 'long'});
   return (
     <S.Home>
         <Menu 
@@ -16,7 +19,7 @@ const Home = () => {
             <S.HomeHeaderDetails>
                 <div>
                     <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
-                    <S.HomeHeaderDetailsDate>Aqui ficará a data</S.HomeHeaderDetailsDate>
+                    <S.HomeHeaderDetailsDate>{dateDescription}</S.HomeHeaderDetailsDate>
                 </div>
                 <S.HomeHeaderDetailsSearch>
                     <Search />
