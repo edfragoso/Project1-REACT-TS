fd669529fb2e89630308775a21b27bd51d96038e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-09-18 15:24:43 -0300

    chore: building login component

---
+++ pizza-fresh-client/app/src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import * as S from "./style";
+
+const Login = () => {
+    return (
+        <S.Login>
+            <S.LoginContent>
+                
+            </S.LoginContent>
+        </S.Login>
+    );
+
+}
+
+export default Login;
