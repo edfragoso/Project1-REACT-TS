a3c39855ce0cd498274612b30cc8ca4b96420b25
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-09-23 18:44:45 -0300

    chore: add route SETTING_PRODUCT

--- pizza-fresh-client/app/src/router.tsx
+++ pizza-fresh-client/app/src/router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import Settings from "pages/Settings/index";
 import Login from "pages/Login";
+import ManageProducts from "components/ManageProducts";
 
 const Router = () => {
     return (
         <Routes>
             <Route path={RoutePath.LOGIN} element={<Login />}/>
             <Route path ={RoutePath.HOME} element={<Settings />}/>
+            <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />}/>
 
         </Routes>
     );
