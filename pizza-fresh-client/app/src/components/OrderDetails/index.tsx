dfed52a80d89c51c6a4a66feb2e19661e17013d8
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-09-27 19:48:44 -0300

    refactor:

--- pizza-fresh-client/app/src/components/OrderDetails/index.tsx
+++ pizza-fresh-client/app/src/components/OrderDetails/index.tsx
@@ -6,57 +6,57 @@ import * as S from "./style";
 import { HTMLAttributes } from "react";
 import { OrderItemType } from "types/OrderItemType";
 
-type OrderDetailsType = HTMLAttributes<HTMLDivElement>
+type OrderDetailsType = HTMLAttributes<HTMLDivElement>;
 
 type OrderDetailsProps = {
-	orders: OrderItemType[];
+  orders: OrderItemType[];
 } & OrderDetailsType;
 
-const OrderDetails = ({orders}: OrderDetailsProps) => {
-	return (
-		<S.OrderDetails>
-			<S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
-			<S.OrderDetailsButtonGroup>
-				<ButtonToggle active={true} value="Comer no Local" />
-				<ButtonToggle active={false} value="P/ Viagem" />
-				<ButtonToggle active={true} value="Delivery" />
-			</S.OrderDetailsButtonGroup>
-			<S.OrderDetailsList>
-				<OrderItemList
-					header={
-						<S.OrderDetailsListTitle>
-							<h4>Item</h4>
-							<h4>Qtd</h4>
-							<h4>Preço</h4>
-						</S.OrderDetailsListTitle>
-					}
-					list={
-						Boolean(orders.length) ? (
-							orders.map((item, index) => (
-								<OrderItem
-								product = {item.product}
-								quantity = {item.quantity}
-								observation = {item.observation}
-								key =  {`OrderDetails-${index}`}
-								/>			
-							))
-						) : (
-							<S.OrderDetailsListGap />
-						)
-					}
-					footer={
-						<S.OrderDetailsListFooter>
-							<S.OrderDetailsListFooterRow>
-								<span>Subtotal</span>
-								<span>R$ 10.00</span>
-							</S.OrderDetailsListFooterRow>
-							<ButtonLarge value="Continue para o pagamento" />
-						</S.OrderDetailsListFooter>
-					}
-				/>
-			</S.OrderDetailsList>
-		</S.OrderDetails>
-	);
+const OrderDetails = ({ orders }: OrderDetailsProps) => {
+  return (
+    <S.OrderDetails>
+      <S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
+      <S.OrderDetailsButtonGroup>
+        <ButtonToggle active={true} value="Comer no Local" />
+        <ButtonToggle active={false} value="P/ Viagem" />
+        <ButtonToggle active={true} value="Delivery" />
+      </S.OrderDetailsButtonGroup>
+      <S.OrderDetailsList>
+        <OrderItemList
+          header={
+            <S.OrderDetailsListTitle>
+              <h4>Item</h4>
+              <h4>Qtd</h4>
+              <h4>Preço</h4>
+            </S.OrderDetailsListTitle>
+          }
+          list={
+            Boolean(orders.length) ? (
+              orders.map((item, index) => (
+                <OrderItem
+                  product={item.product}
+                  quantity={item.quantity}
+                  observation={item.observation}
+                  key={`OrderDetails-${index}`}
+                />
+              ))
+            ) : (
+              <S.OrderDetailsListGap />
+            )
+          }
+          footer={
+            <S.OrderDetailsListFooter>
+              <S.OrderDetailsListFooterRow>
+                <span>Subtotal</span>
+                <span>R$ 10.00</span>
+              </S.OrderDetailsListFooterRow>
+              <ButtonLarge value="Continue para o pagamento" />
+            </S.OrderDetailsListFooter>
+          }
+        />
+      </S.OrderDetailsList>
+    </S.OrderDetails>
+  );
 };
 
 export default OrderDetails;
