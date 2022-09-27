18d2ffc54fad0fb09a7e3a9872c0a9dc2ac92884
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-09-26 22:31:11 -0300

    chore: config login to home navigation

--- pizza-fresh-client/app/src/pages/Login/index.tsx
+++ pizza-fresh-client/app/src/pages/Login/index.tsx
@@ -1,15 +1,26 @@
 import BoxLogin from "components/BoxLogin";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { RoutePath } from "types/routes";
 import * as S from "./style";
 
 const Login = () => {
-    return (
-        <S.Login>
-            <S.LoginContent>
-                <BoxLogin />   
-            </S.LoginContent>
-        </S.Login>
-    );
+  const [errorMesssage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
-}
+  const handleSubmit = () => {
+    navigate(RoutePath.HOME);
+  };
+
+  return (
+    <S.Login>
+      <S.LoginContent>
+        <BoxLogin
+         onSubmitData={handleSubmit}
+         errorMessage={errorMesssage} />
+      </S.LoginContent>
+    </S.Login>
+  );
+};
 
 export default Login;
