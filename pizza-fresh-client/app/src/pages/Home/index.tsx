e553b9cca98659541409caeda1f1b450835e7aa1
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-09-27 21:14:11 -0300

    chore: OrderType

--- pizza-fresh-client/app/src/pages/Home/index.tsx
+++ pizza-fresh-client/app/src/pages/Home/index.tsx
@@ -13,14 +13,18 @@ import { useNavigate } from "react-router-dom";
 import { products } from "mocks/products";
 import { orders } from "mocks/orders";
 import { ProductResponse } from "types/Product";
+import { OrderType } from "types/orderType";
+import { useState } from "react";
 
 const Home = () => {
   const dateDescription = DateTime.now().toLocaleString({
     ...DateTime.DATE_SHORT,
     weekday: "long",
   });
-  
   const navigate = useNavigate();
+  const [activeOrderType, setActiveOrderType] = useState(
+    OrderType.COMER_NO_LOCAL
+  ); 
   const handleNavigation = (path: RoutePath) => navigate(path);
   const handleSelection = (product: ProductResponse) => {}
 
@@ -68,6 +72,8 @@ const Home = () => {
       <aside>
         <OrderDetails
          orders={orders}
+         onChangeActiveOrderType = {(data) => setActiveOrderType(data)}
+         activeOrderType = {activeOrderType}
         />
       </aside>
       {/* <Overlay>
