e034d4ecea267fe71d9c20f512c974cd8c53c1c7
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-10-03 18:55:01 -0300

    chore: Creating the User Service (UserService)

---
+++ pizza-fresh-client/app/src/services/UserService.ts
@@ -0,0 +1,41 @@
+import { Api } from "helpers/endpoints/Api";
+import { endpoint } from "helpers/endpoints";
+import { User, UserUpdate } from "types/api/user";
+
+export const UserService = {
+  getLista: () =>
+    Api(endpoint.listUser(), {
+      method: "GET",
+    }).then((response) => response.json()),
+
+  create: (user: User) =>
+    Api(endpoint.createUser(), {
+      method: "POST",
+      body: JSON.stringify(user),
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json()),
+
+  getById: (id: string) =>
+    Api(endpoint.userById(id), {
+      method: "GET",
+    }).then((response) => response.json()),
+
+  updateById: ({ user, id }: UserUpdate) =>
+    Api(endpoint.productById(id), {
+      method: "PATCH",
+      body: JSON.stringify(user),
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json()),
+
+  deleteById: (id: string) =>
+    Api(endpoint.userById(id), {
+      method: "DELETE",
+    }).then((response) => response.json()),
+   
+};
